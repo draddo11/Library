def72f65cf1ce23b301569d7f6c85ca6a196d80d
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-06-15 23:08:16 +0000

    added admin router

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,17 @@
+const  express = require("express");
+
+const adminRouter = express.Router();
+
+
+function router(nav){
+    adminRouter.route('/')
+
+    .get((req, res) =>{
+        res.send('inserting books')
+    })
+    return adminRouter;
+}
+
+
+
+module.exports= router;
