4cc7cb0548521edadf66780f0fa507572ddb2a8e
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-06-16 00:03:20 +0000

    adding db from mongo

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,7 @@
 const  express = require("express");
-
+const {mongoClient} = require("mongodb");
+const debug =require("debug")("app:adminRoutes");
+// const MongoClient = require("mongodb/lib/mongo_client");
 const adminRouter = express.Router();
 
 
@@ -7,6 +9,22 @@ function router(nav){
     adminRouter.route('/')
 
     .get((req, res) =>{
+        const url = "mongo://localhost:27017"
+        const dbName="LibraryApp";
+
+        (async function mongo(){
+            let client;
+            try{
+                client = await MongoClient.connect(url);
+                debug("Connected correctly to server");
+                const db= client.db(dbName);
+                await db.collection
+
+            }catch(err){
+
+            }
+        }());
+
         res.send('inserting books')
     })
     return adminRouter;
